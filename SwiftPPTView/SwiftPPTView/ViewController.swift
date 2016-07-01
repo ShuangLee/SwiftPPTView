d7724339169fa31e964c88dbe4c0571dab1d1f16
ShuangLee <shawn_acoder@sina.com>
2016-07-01 14:24:05 +0800

    ...

--- SwiftPPTView/SwiftPPTView/ViewController.swift
+++ SwiftPPTView/SwiftPPTView/ViewController.swift
@@ -13,20 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //网络相册：
-        let pptView = SwiftPPTView(type: .Network) { () -> [DataModel]? in
-            var networkImages = ["http://img.netbian.com/file/2015/0619/e8ffa0a298a4f7374df0e599c4fa134d.jpg"]
-            var networkTitleStr = ["网络幻灯:小黄人一","网络幻灯:小黄人二","网络幻灯:小黄人三","网络幻灯:小黄人四","网络幻灯:小黄人五"]
-            var dataModels: [DataModel] = Array()
-            for i in 0..<networkImages.count {
-                let dataModel = DataModel(networkImageUrl: networkImages[i], placeHolderImage: nil, title: networkTitleStr[i],model: nil)
-                dataModels.append(dataModel)
-            }
-            
-            return dataModels
-        }
-        pptView.frame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, 160)
-        view.addSubview(pptView)
+//        //网络相册：
+//        let pptView = SwiftPPTView(type: .Network) { () -> [DataModel]? in
+//            var networkImages = ["http://img.netbian.com/file/2015/0619/e8ffa0a298a4f7374df0e599c4fa134d.jpg"]
+//            var networkTitleStr = ["网络幻灯:小黄人一","网络幻灯:小黄人二","网络幻灯:小黄人三","网络幻灯:小黄人四","网络幻灯:小黄人五"]
+//            var dataModels: [DataModel] = Array()
+//            for i in 0..<networkImages.count {
+//                let dataModel = DataModel(networkImageUrl: networkImages[i], placeHolderImage: nil, title: networkTitleStr[i],model: nil)
+//                dataModels.append(dataModel)
+//            }
+//            
+//            return dataModels
+//        }
+//        pptView.frame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, 160)
+//        view.addSubview(pptView)
         
         
         
@@ -57,7 +57,6 @@ class ViewController: UIViewController {
             
             print(index)
         }
-
     }
 
 }
